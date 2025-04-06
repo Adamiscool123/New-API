dd1cb298066f0d5709083b9928e9e8d21bb8a279
Adamiscool123 <adamidrissi177@gmail.com>
2025-04-06 21:39:45 +0100

    Send new to email

---
+++ Send_email.py
@@ -0,0 +1,28 @@
+import smtplib, ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+
+def send_email(message):
+    email = 'adamidrissi177@gmail.com'
+    password = "gcng gnav kudm rvml"  # Use app-specific password here
+    receiver = 'adamidrissi@gmail.com'
+    host = 'smtp.gmail.com'
+    port = 465
+
+    # Create the email message
+    msg = MIMEMultipart()
+    msg["From"] = email
+    msg["To"] = receiver
+    msg["Subject"] = "Your Article"
+
+    # Attach the message (content) as MIMEText, set to UTF-8 encoding
+    msg.attach(MIMEText(message, "plain", "utf-8"))
+
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(email, password)
+        # Send the email as a string with UTF-8 encoding
+        server.sendmail(email, receiver, msg.as_string())
+
+    print("Email sent successfully")
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from Send_email import send_email
 
 api_key = "c57f44575ba549d787810c575c539e52"
 url = "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=c57f44575ba549d787810c575c539e52"
@@ -9,7 +10,8 @@ request = requests.get(url)
 # Get a dictionary with data
 content = request.json()
 
-# Acess the article titles and description
+# Access the article titles and description
 for content in content["articles"]:
-    print(content["description"])
+    send_email(content["description"])
+
 
