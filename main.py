0b39667be10b5d862c2cbbf2a8be38e89222be0f
Adamiscool123 <adamidrissi177@gmail.com>
2025-04-07 20:46:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ import requests
 from Send_email import send_email
 
 api_key = "c57f44575ba549d787810c575c539e52"
-url = "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=c57f44575ba549d787810c575c539e52"
+url = ("https://newsapi.org/v2/top-headlines?"
+       "country=us&"
+       "category=business"
+       "&apiKey=c57f44575ba549d787810c575c539e52&"
+       "language=en")
 
 # Make request
 request = requests.get(url)
@@ -10,8 +14,10 @@ request = requests.get(url)
 # Get a dictionary with data
 content = request.json()
 
-# Access the article titles and description
-for content in content["articles"]:
-    send_email(content["description"])
+body = ""
+for content in content["articles"][0:20]:
+    if content['title'] is not None:
+        body += str(content["description"]) + "\n" + str(content['url'] + 2*"\n")
+send_email(body)
 
 
